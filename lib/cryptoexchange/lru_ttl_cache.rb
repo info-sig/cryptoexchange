7110945f190447725d10c72838838f27389a2ae7
Borna Novak <dosadnizub@gmail.com>
2018-03-07 23:29:56 +0100

    LruRedux::TTL cache should be thread-safe

--- lib/cryptoexchange/lru_ttl_cache.rb
+++ lib/cryptoexchange/lru_ttl_cache.rb
@@ -2,7 +2,7 @@ module Cryptoexchange
   class LruTtlCache
     class << self
       def ticker_cache(ticker_ttl = 10)
-        @@ticker_cache ||= LruRedux::TTL::Cache.new(100, ticker_ttl)
+        @@ticker_cache ||= LruRedux::TTL::ThreadSafeCache.new(100, ticker_ttl)
       end
     end
   end
